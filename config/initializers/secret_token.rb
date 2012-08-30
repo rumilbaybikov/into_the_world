b06ce02058803e93c07c6a7f4f6bf6f21cbe668f
Rumil <rumil.baybikov@gmail.com>
2012-08-31 01:49:55 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntoTheWorld::Application.config.secret_token = 'd3b00d9426695909a85d3666291f09f1c789b9f730497842483077961c48f2030ef6f0c134c90b59eaca3f1bd645a65b259be9988fa506753e74ee9cbfd461e0'
