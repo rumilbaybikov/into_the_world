9a5c1d6eb8ffef3046e69c16edb1abc0f7e42f81
Rumil <rumil.baybikov@gmail.com>
2012-08-31 12:24:37 +0400

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,4 +61,6 @@ Enki::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.assets.precompile += %w( application.* style.* application.js )
+
 end
